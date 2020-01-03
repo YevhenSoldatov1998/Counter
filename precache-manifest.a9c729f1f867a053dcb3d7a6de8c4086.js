1993090ae467f36c9f8667d4a04bf48724416575
YevhenSoldatov1998 <arr.ess1998@gmail.com>
2020-01-03 13:36:51 +0200

    Updates

---
+++ precache-manifest.a9c729f1f867a053dcb3d7a6de8c4086.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "acb970358bbb9502277bb6be334bd130",
+    "url": "/Counter/index.html"
+  },
+  {
+    "revision": "46fd8f3d06750039a4e9",
+    "url": "/Counter/static/css/main.8ad82c83.chunk.css"
+  },
+  {
+    "revision": "a9f8a8eaa399abda9752",
+    "url": "/Counter/static/js/2.cf7a266e.chunk.js"
+  },
+  {
+    "revision": "e928fe768baa9832b5bc57eae021f30c",
+    "url": "/Counter/static/js/2.cf7a266e.chunk.js.LICENSE"
+  },
+  {
+    "revision": "46fd8f3d06750039a4e9",
+    "url": "/Counter/static/js/main.c3b34f5f.chunk.js"
+  },
+  {
+    "revision": "90be2967693c02a2b753",
+    "url": "/Counter/static/js/runtime-main.bd6b1a70.js"
+  }
+]);
